c6a013aee7cf0cc4981af691e728d161337f943a
vikrambaskar <118271411+VikramBaskar@users.noreply.github.com>
2023-11-20 12:58:11 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,50 @@
+var season = "spring"
+if(season=="spring")
+{
+    console.log("Enjoy the blooming flowers")
+}
+
+if(season=="summer")
+{
+    console.log("Have fun in the sun")
+}
+
+if(season=="autumn")
+{
+    console.log("Admire the colorful leaves")
+}
+ 
+else(season=="winter")
+{
+    console.log("Bundle up and stay warm")
+}
+
+var num=25
+if(num%2==0)
+{
+    console.log("The number is odd")
+}
+else
+{
+    console.log("The num is Even")
+}
+var vowel="e"
+if(vowel=="a"||vowel=="e")
+{
+ console.log("It's a vowel")
+}
+else{
+    console.log("Not an vowel")
+}
+
+var mark=67
+if(mark>30 && mark<80)
+{
+    console.log("good")
+}
+ var numb = 10
+ for(n=1;numb>=n;n++)
+ {
+    console.log(n)
+ }n--
+
